9bf77feb93fa9e651b3c241044ef6e9743b001a1
Edem <augustine.akato@amalitech.com>
2025-04-07 10:02:40 +0000

    first commit

---
+++ src/main/java/org/edem/edasamsns/EdaSamSnsApplication.java
@@ -0,0 +1,13 @@
+package org.edem.edasamsns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdaSamSnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdaSamSnsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/edem/edasamsns/LambdaHandler.java
@@ -0,0 +1,27 @@
+package org.edem.edasamsns;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
+import software.amazon.awssdk.services.sns.SnsClient;
+import software.amazon.awssdk.services.sns.model.PublishRequest;
+
+public class LambdaHandler implements RequestHandler<S3Event, String> {
+    private final String SNS_TOPIC_ARN = System.getenv("SNS_TOPIC_ARN");
+    private final SnsClient snsClient = SnsClient.create();
+
+    @Override
+    public String handleRequest(S3Event event, Context context) {
+        for (var record : event.getRecords()) {
+            String bucket = record.getS3().getBucket().getName();
+            String key = record.getS3().getObject().getKey();
+            String message = String.format("New object uploaded to %s: %s", bucket, key);
+            PublishRequest request = PublishRequest.builder()
+                    .topicArn(SNS_TOPIC_ARN)
+                    .message(message)
+                    .build();
+            snsClient.publish(request);
+        }
+        return "Notification sent";
+    }
+}
