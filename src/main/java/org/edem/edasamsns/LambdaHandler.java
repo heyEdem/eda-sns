641cb0f1b29e7eeb3eea0216d1f9832d7ced7d5b
Edem <augustine.akato@amalitech.com>
2025-04-11 11:47:15 +0000

    fix: lambdahandler method return type to void

--- src/main/java/org/edem/edasamsns/LambdaHandler.java
+++ src/main/java/org/edem/edasamsns/LambdaHandler.java
@@ -6,22 +6,27 @@ import com.amazonaws.services.lambda.runtime.events.S3Event;
 import software.amazon.awssdk.services.sns.SnsClient;
 import software.amazon.awssdk.services.sns.model.PublishRequest;
 
-public class LambdaHandler implements RequestHandler<S3Event, String> {
+public class LambdaHandler implements RequestHandler<S3Event, Void> {
     private final String SNS_TOPIC_ARN = System.getenv("SNS_TOPIC_ARN");
     private final SnsClient snsClient = SnsClient.create();
 
     @Override
-    public String handleRequest(S3Event event, Context context) {
-        for (var record : event.getRecords()) {
-            String bucket = record.getS3().getBucket().getName();
-            String key = record.getS3().getObject().getKey();
-            String message = String.format("New object uploaded to %s: %s", bucket, key);
-            PublishRequest request = PublishRequest.builder()
-                    .topicArn(SNS_TOPIC_ARN)
-                    .message(message)
-                    .build();
-            snsClient.publish(request);
+    public Void handleRequest(S3Event event, Context context) {
+        try {
+            for (var record : event.getRecords()) {
+                String bucket = record.getS3().getBucket().getName();
+                String key = record.getS3().getObject().getKey();
+                String message = String.format("New object uploaded to %s: %s", bucket, key);
+                PublishRequest request = PublishRequest.builder()
+                        .topicArn(SNS_TOPIC_ARN)
+                        .message(message)
+                        .build();
+                snsClient.publish(request);
+            }
         }
-        return "Notification sent";
+        catch (Exception e){
+            context.getLogger().log("Something went wrong " + e);
+        }
+        return null;
     }
 }
